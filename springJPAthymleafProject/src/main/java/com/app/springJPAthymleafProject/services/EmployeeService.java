a60c2a52e285b096c4cf79f7c1d555a446eec103
Vikrant Kambli <136485422+vikrantK8102@users.noreply.github.com>
2025-07-23 22:14:44 +0530

    Delete springJPAthymleafProject directory

--- springJPAthymleafProject/src/main/java/com/app/springJPAthymleafProject/services/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.app.springJPAthymleafProject.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.springJPAthymleafProject.entity.Employee;
-
-public interface EmployeeService extends JpaRepository<Employee, Integer>{
-
-}
